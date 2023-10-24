db4d4834fb8f27d6267214fae32ee0c14e0255a1
Thelman <thelman1991@gmail.com>
2023-10-23 23:20:25 -0400

    delete the eks repo

--- extra/eks-cluster/aws-vpn-gtw.tf
+++
@@ -1,9 +0,0 @@
-# create aws vpn gateway for EKS VPC
-resource "aws_vpn_gateway" "vpn_gw" {
-  vpc_id = "${aws_vpc.eks.id}"
-
-  tags = {
-     "Name" = "eks aws vpn gateway"
-    
-  }
-}
--- extra/eks-cluster/key.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "ec2_key" {
-  key_name   = "ec2_key"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
